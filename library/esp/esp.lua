2be8d3c6dbaf146b53a3407520b581160edfa6f9
Throynw7 <119629638+Throynw7@users.noreply.github.com>
2022-12-01 17:55:36 -0300

    Update esp.lua

--- library/esp/esp.lua
+++ library/esp/esp.lua
@@ -151,8 +151,8 @@ end
 
 -- Main Functions
 function espLibrary.getTeam(player)
-    local team = player.Team;
-    return team, player.TeamColor.Color;
+    local team = game:GetService("Players")[player.Name].PlayerData.TeamValue.Value;
+    return team, Color3.new(0, 0, 1);
 end
 
 function espLibrary.getCharacter(player)
